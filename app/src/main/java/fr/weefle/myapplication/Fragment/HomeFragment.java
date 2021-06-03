97a94e4f3401171c9f633cdd2aba2135c190b302
Weefle <mr.weefle@gmail.com>
2021-06-03 20:00:44 +0200

    Testing influxdb

--- app/src/main/java/fr/weefle/myapplication/Fragment/HomeFragment.java
+++ app/src/main/java/fr/weefle/myapplication/Fragment/HomeFragment.java
@@ -2,7 +2,6 @@ package fr.weefle.myapplication.Fragment;
 
 import android.app.AlertDialog;
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,7 +13,6 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
