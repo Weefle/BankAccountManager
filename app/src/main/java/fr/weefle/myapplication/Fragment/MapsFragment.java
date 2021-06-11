105f1239ea91640fa9362600ffc1b27cf10fb71a
Weefle <mr.weefle@gmail.com>
2021-06-11 15:38:07 +0200

    Added buzzer button

--- app/src/main/java/fr/weefle/myapplication/Fragment/MapsFragment.java
+++ app/src/main/java/fr/weefle/myapplication/Fragment/MapsFragment.java
@@ -1,14 +1,14 @@
 package fr.weefle.myapplication.Fragment;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -16,7 +16,7 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import org.influxdb.InfluxDB;
 import org.influxdb.InfluxDBFactory;
@@ -25,11 +25,20 @@ import org.influxdb.dto.QueryResult;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 import fr.weefle.myapplication.R;
 
 public class MapsFragment extends Fragment {
+
+    private FloatingActionButton button;
 
     private OnMapReadyCallback callback = new OnMapReadyCallback() {
 
@@ -44,8 +53,7 @@ public class MapsFragment extends Fragment {
          */
         @Override
         public void onMapReady(GoogleMap googleMap) {
-            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-            StrictMode.setThreadPolicy(policy);
+
             final String serverURL = "http://178.32.129.132:8086", username = "admin", password = "test1234";
             final InfluxDB influxDB = InfluxDBFactory.connect(serverURL, username, password);
 
@@ -57,7 +65,7 @@ public class MapsFragment extends Fragment {
 
             QueryResult queryResult = influxDB.query(new Query("SELECT * FROM data"));
 
-            System.out.println(queryResult);
+            System.out.println("Result: " + queryResult);
 
             influxDB.close();
             final List<QueryResult.Result> transactions = queryResult.getResults();
@@ -88,7 +96,54 @@ public class MapsFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_maps, container, false);
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        View v = inflater.inflate(R.layout.fragment_maps, container, false);
+        button = v.findViewById(R.id.floatingActionButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                URL url;
+                try {
+                    url = new URL("https://node-red-3-nodered-enabler-prod.eu-b.kmt.orange.com/node-red/api/60a64e580d3494e8489009b7/button");
+
+                    HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                    con.setRequestMethod("POST");
+                    con.setRequestProperty("Content-Type", "application/json");
+                    con.setRequestProperty("Authorization", "Basic bm9kZS1yZWQtYXBpOmNyUVIxTlZ2TGJpdUtXN1BzSnR2dHloYlpYVk1rb0dY");
+                    con.setConnectTimeout(5000);
+                    con.setReadTimeout(5000);
+                    con.setInstanceFollowRedirects(true);
+                    String hello = "{\"username\":\"root\",\"password\":\"password\"}";
+                    byte[] out = hello.getBytes(StandardCharsets.UTF_8);
+                    con.connect();
+                    try(OutputStream os = con.getOutputStream()) {
+                        os.write(out);
+                    }
+                    int status = con.getResponseCode();
+                    if (status == HttpURLConnection.HTTP_MOVED_TEMP
+                            || status == HttpURLConnection.HTTP_MOVED_PERM) {
+                        String location = con.getHeaderField("Location");
+                        URL newUrl = new URL(location);
+                        con = (HttpURLConnection) newUrl.openConnection();
+                    }
+                    BufferedReader in = new BufferedReader(
+                            new InputStreamReader(con.getInputStream()));
+                    String inputLine;
+                    StringBuilder content = new StringBuilder();
+                    while ((inputLine = in.readLine()) != null) {
+                        content.append(inputLine);
+                    }
+                    in.close();
+                    con.disconnect();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+        return v;
     }
 
    @Override
